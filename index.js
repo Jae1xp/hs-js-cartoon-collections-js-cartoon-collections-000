8a9665cf26a46b62cb2f2d9c7be02ab23b15162a
Jae Son <jae1xp@gmail.com>
2018-03-14 13:59:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
+var dwarfNames = ["Doc", "Dopey", "Bashful", "Grumpy"];
+
 function dwarfRollCall(dwarves) {
+	var newStr = "";
+	for(var i = 0; i < dwarves.length; i++) {
+		if(i === dwarves.length) {
+			newStr += (i + 1 + ". " + dwarves[i]);
+		} else {
+			newStr += (i + 1 + ". " + dwarves[i] + " ");
+		}
+	}
+	return newStr;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+function summonCaptainPlanet (calls) {
+  for (var i = 0; i < calls.length; i++) {
+    calls[i] = calls[i].toUpperCase() + "!";
+  }
+  console.log(calls);
+  return calls
 }
 
+
+var shortWords = ["wind", "fire"];
+var longWords = ["earth", "water", "heart"];
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
-}
+
+var	snacks = ["crackers", "gouda", "thyme"];
+
+var soup = ["tomato soup", "oyster", "crackers"];
+
+var cheeses = ["gouda", "cheddar", "camembert"];
+
+  function findTheCheese (foods) {
+    for (var i = 0; i < foods.length; i++) {
+      if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+        return foods[i];
+      }
+    }
+    return "no cheese!";
+  }
